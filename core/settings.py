b8bf6d9a1d277088f8b48b7537efaeff6bb7cf7b
Francisco <vg.bassoonist@gmail.com>
2022-04-01 17:18:55 -0400

    Small updates

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
     
     'vgb_blog.apps.VgbBlogConfig',
     'taggit',
@@ -82,8 +83,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'VGB',
+        'USER': 'vgb',
+        'PASSWORD': 'PGS-/2022'
     }
 }
 
