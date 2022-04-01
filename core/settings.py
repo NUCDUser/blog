942c75efd859edd9591208693f943ab8256e1533
Francisco <vg.bassoonist@gmail.com>
2022-04-01 17:57:52 -0400

    Added django-decouple

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^71p40=gt%&+aux4h5kyju9^fn0&i5mh%2628)$g5#*%e3-8f'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -84,9 +85,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'VGB',
-        'USER': 'vgb',
-        'PASSWORD': 'PGS-/2022'
+        'NAME': config('PG_DB_NAME'),
+        'USER': config('PG_DB_USER'),
+        'PASSWORD': config('PG_DB_PASSWORD')
     }
 }
 
